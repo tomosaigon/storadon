84e84174610a4276b94e79ad8acaf1ca3ee20a12
tomo <tomo@crowdeo.com>
2023-02-06 11:06:27 +0700

    feat: older, recent

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
 # storadon
-A protocol for preserving Mastodon communities for eternity.
+### A protocol for preserving Mastodon communities for eternity.
 
+# introduction
+Much of our Internet existence is encapsulated in our collective posts on social media, especially between members of a community who have a long history of interacting with each other on centrally controlled servers like, Twitter or Mastodon. One problem with any centrally managed server is that the server is a single point of failure, which leads to all the content stored on it to be lost forever or delete it on purpose. This could be years of messages and threads for thousands of community members.
 
+The problem with Twitter is that the company is hostile to third-party access of their data via API, which makes it difficult or impossible to archive community history even if you could solve the problem of defining the communities members as a subset of the billions of Twitter users. This is why Mastodon is an ideal server implementation for archiving communities' history. Our protocol takes advantage of the Mastodon API to create a standard archive format that can be updated and then uses Lighthouse to store the data to be accessed by anyone at any time in the future, regardless of the status of the original Mastodon server which may go down temporarily or permanently as happens all the time. 
+
+# setup 
+Clone this repo, and run:
+```
 $ npm i
+```
 
+Create an account on Lighthouse and generate an API key and copy it. We test with the Polygon Mumbai testnet. For the time being, you don't need to top up any funds as you will receive 1 gigabyte of storage with your account.
+```
 $ export API_KEY=...
+$ source lhapikey.sh
+
+$ rm store/*
 
 $ node main.js init
+
+$ node main.js older
+```
+This will fetch the most recent tweets, and then the ones slightly older, and save them all to files in the store, upload them to IPFS, using Lighthouse, and save the resulting CID. Lighthouse will take care of creating a deal on Filecoin, and ensure it is stored forever.
+
+```
+curl https://gateway.lighthouse.storage/ipfs/$(cat $(ls store/*.cid | tail -1)) | jq '.[]|.content'
+```
+
--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const STORE = './store'
 const LIMIT = 40;
 const argv = yargs
   .command('init', 'Initialize the oldest and newest post ID files', {}, init)
+  .command('older', 'Fetch and save 40 posts older than the current oldest post', {}, fetchOlderPosts)
+  .command('recent', 'Fetch and save 40 most recent posts at a time', {}, fetchRecentPosts)
   .help()
   .argv;
 
@@ -42,6 +44,76 @@ async function init() {
   }
 }
 
+async function fetchRecentPosts() {
+  try {
+    // let maxId = BigInt(fs.readFileSync('oldest', 'utf-8'));
+    // maxId -= 1n;
+    // console.log(maxId)
+    let min_id = findMostRecentId();
+    let max_id = ''; // HACK
+    // let overwrittenNewest = false;
+    // gets 
+    while (1) {
+      const response = await axios.get(`https://${SERVER}/api/v1/timelines/public`, {
+        params: {
+          limit: LIMIT,
+          max_id,
+          min_id,
+          local: 'true'
+        }
+      })
+      console.log(`Fetching URL: ${response.request.res.responseUrl}`);
+      // console.log(response)
+      const posts = response.data;
+      if (posts.length == 0) {
+        break;
+      }
+      let oldestId = BigInt(posts[posts.length - 1].id);
+      // let newestId = BigInt(posts[0].id);
+      max_id = oldestId;
+      // fs.writeFileSync('oldest', oldestId.toString());
+      // if (!overwrittenNewest) {
+      //   fs.writeFileSync('newest', newestId.toString());
+      //   overwrittenNewest = true;
+      // }
+
+      storePosts(posts);
+
+      // // posts.forEach(post => console.log(post.id, post.created_at));
+      // const firstPostCreatedAt = new Date(posts[0].created_at).toISOString();
+      // const lastPostCreatedAt = new Date(posts[posts.length - 1].created_at).toISOString();
+      // const fileName = `${SERVER}-${firstPostCreatedAt}-${lastPostCreatedAt}.json`;
+      // fs.writeFileSync(fileName, JSON.stringify(posts), 'utf-8');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchOlderPosts() {
+  try {
+    const maxId = findOldestId() - 1n;
+    const response = await axios.get(`https://${SERVER}/api/v1/timelines/public`, {
+      params: {
+        limit: LIMIT,
+        max_id: maxId,
+        local: 'true'
+      }
+    });
+    console.log(`Fetching URL: ${response.request.res.responseUrl}`);
+    const posts = response.data;
+    storePosts(posts);
+    // const firstPostCreatedAt = new Date(posts[0].created_at).toISOString();
+    // const lastPostCreatedAt = new Date(posts[posts.length - 1].created_at).toISOString();
+    // const fileName = `${SERVER}-${firstPostCreatedAt}-${lastPostCreatedAt}.json`;
+    // fs.writeFileSync(fileName, JSON.stringify(posts), 'utf-8');
+    // console.log(`Saved posts to ${fileName}`);
+    // upload(fileName);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 async function storePosts(posts) {
   const firstPostCreatedAt = new Date(posts[0].created_at).toISOString();
   const lastPostCreatedAt = new Date(posts[posts.length - 1].created_at).toISOString();
@@ -59,4 +131,34 @@ async function upload(fileName) {
   // console.log(response);
   console.log("Visit at: https://gateway.lighthouse.storage/ipfs/" + response.data.Hash);
   fs.writeFileSync(fileName.replace('json', 'cid'), response.data.Hash, 'utf-8');
+}
+
+function findMostRecentId() {
+  const files = fs.readdirSync(STORE).filter(file => file.startsWith(SERVER) && file.endsWith('.json'));
+  let mostRecentId = 0n;
+  for (const file of files) {
+    const contents = JSON.parse(fs.readFileSync(STORE + '/' + file, 'utf-8'));
+    for (const post of contents) {
+      const id = BigInt(post.id);
+      if (id > mostRecentId) {
+        mostRecentId = id;
+      }
+    }
+  }
+  return mostRecentId;
+}
+
+function findOldestId() {
+  const files = fs.readdirSync(STORE).filter(file => file.startsWith(SERVER) && file.endsWith('.json'));
+  let oldestId = 109811390171541037n; //XXX choose sane BigInt(Number.MAX_SAFE_INTEGER);
+  for (const file of files) {
+    const contents = JSON.parse(fs.readFileSync(STORE + '/' + file, 'utf-8'));
+    for (const post of contents) {
+      const id = BigInt(post.id);
+      if (id < oldestId) {
+        oldestId = id;
+      }
+    }
+  }
+  return oldestId;
 }
